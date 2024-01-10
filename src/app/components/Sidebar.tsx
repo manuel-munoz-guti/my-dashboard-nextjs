7c27843402ee913b4f4fbf0831f77ceabb38b0ad
Jose Manuel Munoz Gutierrez <manuelinux@gmail.com>
2024-01-09 23:59:21 -0400

    Adding an static profile image

--- src/app/components/Sidebar.tsx
+++ src/app/components/Sidebar.tsx
@@ -1,8 +1,8 @@
+import Image from "next/image"
 
 export const Sidebar = () => {
     return (
-        <div 
-            id="menu" 
+        <div id="menu" 
             style={{ width: '400px' }}
             className="bg-gray-900 min-h-screen z-10 text-slate-300 w-64 left-0 h-screen overflow-y-scroll"
         >
@@ -15,7 +15,12 @@ export const Sidebar = () => {
                 <p className="text-slate-500">Welcome back,</p>
                 <a href="#" className="inline-flex space-x-2 items-center">
                     <span>
-                        <img className="rounded-full w-8 h-8" src="https://images.unsplash.com/photo-1542909168-82c3e7fdca5c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=128&q=80" alt="" />
+                        <Image className="rounded-full w-8 h-8" 
+                            src="https://images.unsplash.com/photo-1542909168-82c3e7fdca5c" 
+                            alt="User avatar"
+                            width={50}
+                            height={50}
+                        />
                     </span>
                     <span className="text-sm md:text-base font-bold">
                         Manuel Munoz
